29041c031b138ab9c2cadf394dc6fffad4000812
zim0101 <cybertronix.4406@gmail.com>
2020-08-16 17:28:54 +0600

    Add exercise for ext2 of cha2

--- src/main/java/com/masterdevskills/cha2/ext2/DidThatWork.java
+++ src/main/java/com/masterdevskills/cha2/ext2/DidThatWork.java
@@ -5,7 +5,8 @@ import com.masterdevskills.cha2.ext2.service.InMemoryMovieService;
 
 public class DidThatWork {
 	public static void main(String[] args) {
-		var allMovies = InMemoryMovieService.getInstance().findAllMovies();
+		var allMovies = InMemoryMovieService.getInstance()
+				.findAllMovies();
 		System.out.println("Total Movies: " + allMovies.size());
 	}
 }
